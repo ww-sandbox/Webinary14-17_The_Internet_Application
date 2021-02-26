24d678e1776356f2fc35b3ef417131da870d00b1
Wojciech Wroblewski <wojciech.wroblewski@intive.com>
2021-02-26 15:15:47 +0100

    dostosowanie do Webinaru15

--- src/test/java/ActionsTests.java
+++ src/test/java/ActionsTests.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,10 +13,12 @@ import org.testng.annotations.Test;
 public class ActionsTests extends BaseTest{
     @Test
     public void openHoversPage(){
+        driver.get("http://the-internet.herokuapp.com/");
         WebElement hoversLink = driver.findElement(By.linkText("Hovers"));
         //driver.findElement(By.xpath("//a[text()='Hovers']"));
         hoversLink.click();
         String headerText = driver.findElement(By.tagName("h3")).getText();
+
         Assert.assertEquals(headerText, "Hovers");
     }
 
@@ -25,8 +30,14 @@ public class ActionsTests extends BaseTest{
         - odnajdź obrazek profilu drugiego użytkownika
         - najedź na obrazek
         - zlokalizuj link 'View profile' i kliknij w niego
-        - sprawdź czy adres strony jest poprawny - zawiera '/users/2'
+        Stwórz wait i poczekaj aż adres strony będzie zawierał "/users/2"
          */
+        Actions openUserProfileAction = new Actions(driver);
+        WebElement secondUserAvatar = driver.findElement(By.cssSelector(".figure:nth-of-type(2)"));
+        openUserProfileAction.moveToElement(secondUserAvatar).pause(1000).moveToElement(secondUserAvatar.findElement(By.tagName("a"))).click().build().perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlContains("/users/2"));
     }
 
 }
--- src/test/java/LocatorsTests.java
+++ src/test/java/LocatorsTests.java
@@ -1,6 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LocatorsTests extends BaseTest{
@@ -10,31 +7,11 @@ public class LocatorsTests extends BaseTest{
         //Wypełnij pola formularza (skorzystaj z By.id)
         //Kliknij na przycisk (skorzystaj z By.tagName)
         //Sprawdź czy wyświetlony został porpawny komunikat o zalogowaniu (By.cssSelector)
-        driver.get("http://the-internet.herokuapp.com/login");
-        WebElement usernameInput = driver.findElement(By.id("username"));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.tagName("button"));
-
-        usernameInput.sendKeys("tomsmith");
-        passwordInput.sendKeys("SuperSecretPassword!");
-        loginButton.click();
-
-        WebElement successAlert = driver.findElement(By.cssSelector(".flash.success"));
-        String successText = successAlert.getText();
-
-        Assert.assertTrue(successText.contains("You logged into a secure area!"));
     }
 
     @Test
     public void logoutTest(){
         //Wyloguj się korzystając z By.className
         //Sprawdź czy wyświetlony został porpawny komunikat o wylogowaniu (By.xpath)
-        WebElement logoutButton = driver.findElement(By.className("icon-signout"));
-        logoutButton.click();
-
-        WebElement successAlert = driver.findElement(By.cssSelector(".flash.success"));
-        String successText = successAlert.getText();
-
-        Assert.assertTrue(successText.contains("You logged out of the a secure area!"));
     }
 }
