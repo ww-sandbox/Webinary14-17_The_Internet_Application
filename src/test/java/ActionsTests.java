77e6b732421288817c5b5c4f390585c4f6074acc
Wojciech Wroblewski <wojciech.wroblewski@intive.com>
2021-02-26 15:28:45 +0100

    backward comp

--- src/test/java/ActionsTests.java
+++ src/test/java/ActionsTests.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -25,8 +26,17 @@ public class ActionsTests extends BaseTest{
         - odnajdź obrazek profilu drugiego użytkownika
         - najedź na obrazek
         - zlokalizuj link 'View profile' i kliknij w niego
-        - sprawdź czy adres strony jest poprawny - zawiera '/users/2'
+        Znajdź element 'h1' z tekstem 'Not Found'
+        Sprawdź czy aktualny url zawiera '/users/2'
          */
+        Actions openUserProfileAction = new Actions(driver);
+        WebElement secondUserAvatar = driver.findElement(By.cssSelector(".figure:nth-of-type(2)"));
+        openUserProfileAction.moveToElement(secondUserAvatar).pause(1000).moveToElement(secondUserAvatar.findElement(By.tagName("a"))).click().build().perform();
+
+        driver.findElement(By.xpath("//h1[text()='Not Found']"));
+        String currentUrl = driver.getCurrentUrl();
+
+        Assert.assertTrue(currentUrl.contains("/users/2"));
     }
 
 }
