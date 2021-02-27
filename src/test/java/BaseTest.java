152f49b754bf5c23699724f8e6e9f064ba2e0ce1
Wojciech Wroblewski <wojciech.wroblewski@intive.com>
2021-02-27 16:54:08 +0100

    dodane zadania

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import utils.ScreenshotUtil;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -16,9 +20,19 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
+    //Dodaj metodę 'methodTearDown' z adnotacją '@AfterMethod' pobierającą jako argument 'ITestResult result'
+    //Zapisującą zrzut ekranu w przypadku testu zakończonego failem
+    @AfterMethod
+    public void methodTearDown(ITestResult result) throws IOException {
+        if(result.getStatus() != ITestResult.SUCCESS){
+            String screenshotName = "screenshots/" + result.getName() + result.getEndMillis() + ".png";
+            ScreenshotUtil.takeScreenshot(driver, screenshotName);
+        }
+    }
+
     @AfterClass
     public void tearDown(){
         driver.quit();
-        //Dodaj kod zapisujący screenshot w przypadku 'faila' testu
     }
 }
